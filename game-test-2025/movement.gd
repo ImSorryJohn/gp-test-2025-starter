2a06b12e71cac4418a3b78bfd2167a99ffa7df50
ImSorryJohn <C24730405@myTUDublin.ie>
2025-05-15 13:23:20 +0100

    2nd Stage, almost there

--- game-test-2025/movement.gd
+++ game-test-2025/movement.gd
@@ -1,9 +1,19 @@
 extends CharacterBody2D
 
+const SPEED = 100
+
+
 func _ready() -> void:
 
 	pass 
 func _physics_process(delta: float) -> void:
+	
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 	pass
 
 
