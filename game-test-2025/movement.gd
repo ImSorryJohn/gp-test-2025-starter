ae51cdb190de1b55da37197ac637e9f744fa9554
ImSorryJohn <C24730405@myTUDublin.ie>
2025-05-15 13:21:11 +0100

    1st stage of grief lol

--- game-test-2025/movement.gd
+++ game-test-2025/movement.gd
@@ -1,26 +1,9 @@
 extends CharacterBody2D
-class_name Player
-
-const SPEED = 300.0
-@export var move_left:String
-@export var move_right:String
-@export var move_up:String
-@export var move_down:String
-@export var lives:int = 5
 
 func _ready() -> void:
-	
-	
-	
+
 	pass 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	
 	pass
 
 
