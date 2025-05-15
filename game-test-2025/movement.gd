493cd53bc88b605b937a45716d8f20cefcbe0b75
ImSorryJohn <C24730405@myTUDublin.ie>
2025-05-15 13:10:43 +0100

    the big confusion

--- game-test-2025/movement.gd
+++ game-test-2025/movement.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 class_name Player
 
-@export var speed:float = 100
+const SPEED = 300.0
 @export var move_left:String
 @export var move_right:String
 @export var move_up:String
@@ -13,7 +13,15 @@ func _ready() -> void:
 	
 	
 	pass 
-
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	
+	pass
 
 
 func _process(delta: float) -> void:
